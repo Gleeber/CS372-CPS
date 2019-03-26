b6d5219d956efd3c678312f83b06dbb48a320c19
jaarhus <jonathan.aarhus@gmail.com>
2019-03-26 14:27:51 -0800

    Add require for center point.

--- cpsTest.cpp
+++ cpsTest.cpp
@@ -8,10 +8,12 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "shape.h"
+#include <utility>
+using std::pair;
 
 TEST_CASE("Test for shape class:") {
 	Shape testShape;
-
+	REQUIRE(testShape.getMidPoint() == auto std::make_pair(306, 396))
 	SECTION("") {
 		
 	}
