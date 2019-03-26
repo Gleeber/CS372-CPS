a993aaa067685c8aecced132fd2e46ba6518d57f
jaarhus <jonathan.aarhus@gmail.com>
2019-03-26 14:17:09 -0800

    Begin Creating tests for Shape class.

---
+++ cpsTest.cpp
@@ -0,0 +1,18 @@
+//cpsTest.cpp
+//cs372 spring 201
+//Project 2
+//Team members: Jonathan Aarhus, Andrew Adler, Ian Ferguson
+
+//Test File for CPS (C++ to PostScript)
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "shape.h"
+
+TEST_CASE("Test for shape class:") {
+	Shape testShape;
+
+	SECTION("") {
+		
+	}
+}
