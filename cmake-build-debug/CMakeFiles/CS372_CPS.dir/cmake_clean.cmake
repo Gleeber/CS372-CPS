d82145663fdf5b82c75eb23cc16f9d6e0fe1e326
Andrew Adler <gleeb101@gmail.com>
2019-03-28 13:50:10 -0800

    Revert "Remove unnecessary CLion files"
    This reverts commit 306b2fe8f61a4762f3b6a9a4d465a69641640e72.

---
+++ cmake-build-debug/CMakeFiles/CS372_CPS.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS372_CPS.dir/shape.cpp.o"
+  "CMakeFiles/CS372_CPS.dir/cpsTest.cpp.o"
+  "CS372_CPS.pdb"
+  "CS372_CPS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS372_CPS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpsTest.cpp
@@ -0,0 +1,46 @@
+//cpsTest.cpp
+//cs372 spring 201
+//Project 2
+//Team members: Jonathan Aarhus, Andrew Adler, Ian Ferguson
+
+//Test File for CPS (C++ to PostScript)
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "shape.h"
+#include <utility>
+using std::pair;
+
+TEST_CASE("Test for Shape class:") {
+	SECTION("Test default constructor:") {
+		Shape testShape;
+		REQUIRE(testShape.getCenter() == std::make_pair(306, 396));
+		REQUIRE(testShape.getHeight() == 72);
+		REQUIRE(testShape.getWidth() == 72);
+	}
+}
+
+TEST_CASE("Test for Circle class:")
+{
+	SECTION("Test default constructor:") {
+		Circle testCircle;
+		REQUIRE(testCircle.getCenter() == std::make_pair(306, 396));
+		REQUIRE(testCircle.getHeight() == 72);
+		REQUIRE(testCircle.getWidth() == 72);
+		REQUIRE(testCircle.getRadius() == 36);
+	}
+}
+
+TEST_CASE("Test for Rectangle class:") {
+	Rectangle testRectangle;
+	REQUIRE(testRectangle.getCenter() == std::make_pair(306, 396));
+	REQUIRE(testRectangle.getHeight() == 72);
+	REQUIRE(testRectangle.getWidth() == 144);
+
+	SECTION("Test default constructor:") {
+		Rectangle testRectangle;
+		REQUIRE(testShape.getCenter() == std::make_pair(306, 396));
+		REQUIRE(testShape.getHeight() == 72);
+		REQUIRE(testShape.getWidth() == 144);
+	}
+}
