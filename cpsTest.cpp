acee93335b178118fdeedcf825a43348922ca229
jaarhus <jonathan.aarhus@gmail.com>
2019-03-30 15:37:21 -0800

    Add test case for default polygon.

--- cpsTest.cpp
+++ cpsTest.cpp
@@ -9,12 +9,12 @@
 #include "catch.hpp"
 #include "shape.h"
 #include <utility>
-using std::pair;
+using std::make_pair;
 
 TEST_CASE("Test for Shape class:") {
 	SECTION("Test default constructor:") {
 		Shape testShape;
-		REQUIRE(testShape.getCenter() == std::make_pair(306.0, 396.0));
+		REQUIRE(testShape.getCenter() == make_pair(306.0, 396.0));
 		REQUIRE(testShape.getHeight() == 72);
 		REQUIRE(testShape.getWidth() == 72);
 	}
@@ -27,7 +27,7 @@ TEST_CASE("Test for Circle class:")
 {
 	SECTION("Test default constructor:") {
 		Circle testCircle;
-		REQUIRE(testCircle.getCenter() == std::make_pair(306.0, 396.0));
+		REQUIRE(testCircle.getCenter() == make_pair(306.0, 396.0));
 		REQUIRE(testCircle.getHeight() == 72);
 		REQUIRE(testCircle.getWidth() == 72);
 		REQUIRE(testCircle.getRadius() == 36);
@@ -37,7 +37,7 @@ TEST_CASE("Test for Circle class:")
 TEST_CASE("Test for Rectangle class:") {
 	SECTION("Test default constructor:") {
 		//Rectangle testRectangle;
-		//REQUIRE(testRectangle.getCenter() == std::make_pair(306, 396));
+		//REQUIRE(testRectangle.getCenter() == make_pair(306, 396));
 		//REQUIRE(testRectangle.getHeight() == 72);
 		//REQUIRE(testRectangle.getWidth() == 144);
 	}
@@ -46,7 +46,7 @@ TEST_CASE("Test for Rectangle class:") {
 TEST_CASE("Test for Triangle:") {
 	SECTION("Test default constructor:") {
 		//Triangle testTriangle;
-		//REQUIRE(testTriangle.getCenter() == std::make_pair(306, 396));
+		//REQUIRE(testTriangle.getCenter() == make_pair(306, 396));
 		//REQUIRE(testTriangle.getHeight() == 72);
 		//REQUIRE(testTriangle.getWidth() == 72);
 	}
@@ -55,8 +55,17 @@ TEST_CASE("Test for Triangle:") {
 TEST_CASE("Test for Square:") {
 	SECTION("Test default constructor") {
 		//Square testSquare;
-		//REQUIRE(testSquare.getCenter() == std::make_pair(306, 396));
+		//REQUIRE(testSquare.getCenter() == make_pair(306, 396));
 		//REQUIRE(testSquare.getHeight() == 72);
 		//REQUIRE(testSquare.getWidth() == 72);
+	}
+}
+
+TEST_CASE("Test for Pentagon:") {
+	SECTION("Test default constructor:") {
+		//Polygon testDefaultPolygon;
+		//REQUIRE(testDefaultPolygon.getCenter() == make_pair(306, 396));
+		//REQUIRE(testDefaultPolygon.getHeight() == 72);
+		//REQUIRE(testDefaultPolygon.getWidth() == 72);
 	}
 }
